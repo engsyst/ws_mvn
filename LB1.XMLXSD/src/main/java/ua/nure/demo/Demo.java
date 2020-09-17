ab2b825ca5b3eab6549b54f9fca691b54586855e
engsyst <engsyst@gig>
2020-09-17 20:09:21 +0300

    LB1 pom.xml added

---
+++ LB1.XMLXSD/src/main/java/ua/nure/demo/Demo.java
@@ -0,0 +1,8 @@
+package ua.nure.demo;
+
+public class Demo {
+
+	public static void main(String[] args) throws Exception {
+//		XSLTransform.main(new String[] {"orders.xsl", "orders.xml", "orders.html"});
+	}
+}
