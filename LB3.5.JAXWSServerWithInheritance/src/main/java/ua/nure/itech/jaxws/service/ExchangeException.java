eb90bef0a319beb01641887fa2c25e6d24e630de
Iurii Mishcheriakov <eng.syst@gmail.com>
2022-09-30 13:24:47 +0300

    Remove temporary projects

--- LB3.5.JAXWSServerWithInheritance/src/main/java/ua/nure/itech/jaxws/service/ExchangeException.java
+++
@@ -1,5 +0,0 @@
-package ua.nure.itech.jaxws.service;
-
-public class ExchangeException extends Exception {
-	private static final long serialVersionUID = 1L;
-}
--- LB3.5.JAXWSServerWithInheritance/src/main/java/ua/nure/itech/jaxws/service/HelloMessenger.java
+++
@@ -1,8 +0,0 @@
-package ua.nure.itech.jaxws.service;
-
-import javax.jws.WebService;
-
-@WebService
-public interface HelloMessenger extends Exchanger {
-	String hello(String name);
-}
