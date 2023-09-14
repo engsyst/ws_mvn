08a841733b85c61767357ea0a5ce73486bf22ce0
Iurii Mishcheriakov <eng.syst@gmail.com>
2023-09-14 23:45:08 +0300

    LB1 organize Transformer main()

--- LB1.XMLXSD/src/main/java/ua/nure/it/demo/TransformerDemo.java
+++ LB1.XMLXSD/src/main/java/ua/nure/it/demo/TransformerDemo.java
@@ -3,6 +3,10 @@ package ua.nure.it.demo;
 public class TransformerDemo {
 
 	public static void main(String[] args) throws Exception {
-		XSLTransform.main(new String[] {"xml/xslt/books.xsl", "xml/xslt/books.xml", "xml/xslt/books.html"});
+		XSLTransform.main(new String[] {"xml/xslt/books/books_xml2xml.xsl", "xml/xslt/books/books.xml", "xml/xslt/bookscatalog.xml"});
+		XSLTransform.main(new String[] {"xml/xslt/cd/cd.xsl", "xml/xslt/cd/cd.xml", "xml/xslt/cd.html"});
+		XSLTransform.main(new String[] {"xml/xslt/cd/cd2.xsl", "xml/xslt/cd/cd.xml", "xml/xslt/cd2.html"});
+		XSLTransform.main(new String[] {"xml/xslt/content/content.xsl", "xml/xslt/content/content.xml", "xml/xslt/content.html"});
+		XSLTransform.main(new String[] {"xml/xslt/nums/nums.xsl", "xml/xslt/nums/nums.xml", "xml/xslt/nums.html"});
 	}
 }
